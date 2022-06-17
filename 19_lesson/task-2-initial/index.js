e94c9e977aeeff28b0e0e21fe597f2ad8c651735
mrCtik <99313450+mrCtik@users.noreply.github.com>
2022-06-17 21:29:23 +0700

    19 lesson task-2

--- 19_lesson/task-2-initial/index.js
+++ 19_lesson/task-2-initial/index.js
@@ -1,1 +1,17 @@
 import './index.css';
+
+const newButton = document.querySelector('.cookie-consent__button');
+
+const cookie = () => { 
+    const cookieStatus = localStorage.getItem('cookie');
+    const cookieHTML = document.querySelector('.cookie-consent');
+    cookieHTML.style.display = cookieStatus;  
+}
+
+cookie();
+
+newButton.onclick = function() {
+    console.log('click');
+    localStorage.setItem('cookie', 'none');
+    cookie();
+};
